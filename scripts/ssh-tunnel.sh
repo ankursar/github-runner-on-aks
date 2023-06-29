095de47bfa512aa11613f1b2a8319308a1ec9b8b
ankursar <ankur_saraswat@epam.com>
2023-06-29 19:56:35 +0530

    Updated workflow

--- scripts/ssh-tunnel.sh
+++ scripts/ssh-tunnel.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-HOP_HOST="[-i <priv_key_file_path> ]<user>@<hop_host>"
-TARGET_HOST=<target_host>
-TARGET_PORT=22
+SSH_PUBLIC_KEY=SSH_PUBLIC_KEY
+SSH_USER=SSH_USER
+SSH_HOST=SSH_HOST
 
-PORT_FORWARDING="1$TARGET_PORT:$TARGET_HOST:$TARGET_PORT"
-PID=$(/bin/ps -ef |grep "$PORT_FORWARDING" |grep -v grep |awk '{print $2}')
-if [[ -z $PID ]]
-then
-    /usr/bin/ssh -fN -g -L $PORT_FORWARDING $HOP_HOST
-else
-    echo -e "\nThe SSH tunnel is already running with process ID => ${PID}\n"
-    exit 1
-fi
-
-PID=$(/bin/ps -ef |grep "$PORT_FORWARDING" |grep -v grep |awk '{print $2}')
-echo -e "\nFanfare! SSH tunnel up & running in the local port 1$TARGET_PORT!"
-echo -e "\nProcess ID of SSH tunnel => ${PID}\n"
+ssh -i $SSH_PUBLIC_KEY -f -N -L localhost:80:10.0.1.4:80 $SSH_USER@$SSH_HOST
